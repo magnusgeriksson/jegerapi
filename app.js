56ef481e0180a85ccf2edb9605fccc5ecd6392d8
Magnus Gaustad Eriksson <magnus.eriksson@bouvet.no>
2019-11-11 12:35:40 +0100

    init

---
+++ app.js
@@ -0,0 +1,80 @@
+import express from 'express';
+import { jegerinfo, skyteprover, vaapen, proveskudd } from './db/db';
+import bodyParser from 'body-parser';
+
+
+// Set up the express app
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+// get all todos
+app.get('/api/getjegerinfo/', (req, res) => {
+  res.status(200).send({
+    jegerinfo: jegerinfo
+  })
+});
+
+app.post('/api/addskyteprove/', (req, res) => {
+    if(!req.body.dato) {
+        return res.status(400).send({
+            success: 'false',
+            message: 'Mangler dato'
+        })
+    }
+    if(!req.body.vaapennr) {
+        return res.status(400).send({
+            success: 'false',
+            message: 'Mangler våpennr'
+        })
+    }
+  
+    if(!vaapen.find((v)=> v.vaapennr === req.body.vaapennr)){
+        return res.status(400).send({
+            success: 'false',
+            message: 'Våpennummer finnes ikke på brukeren'
+        })
+    }
+    const skyteprove = {
+        dato: req.body.dato,
+        vaapennr: req.body.vaapennr,
+        godkjent: true
+    }
+    skyteprover.push(skyteprove)
+    return res.status(201).send({
+        success: 'true',
+        message: 'Skyteprøve lagt til'
+    })
+    
+});
+
+app.post('/api/addproveskudd', (req, res) => {
+    if(!req.body.dato) {
+        return res.status(400).send({
+            success: 'false',
+            message: 'Mangler dato'
+        })
+    }
+    if(!req.body.antall) {
+        return res.status(400).send({
+            success: 'false',
+            message: 'Mangler antall'
+        })
+    }
+
+    const ps = {
+        antall: Number(req.body.antall),
+        dato: req.body.dato
+    }
+    proveskudd.push(ps);
+    return res.status(201).send({
+        success: 'true',
+        message: 'Prøveskudd lagt til'
+    })
+})
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`)
+});
---
+++ db/db.js
@@ -0,0 +1,47 @@
+export const skyteprover = [{
+    "dato": "2019-09-24",
+    "vaapennr": "15461234",
+    "godkjent": true
+}]
+
+export const proveskudd = [{
+    "antall": 15,
+    "dato": "2019-09-24"
+},
+{
+    "antall": 5,
+    "dato": "2019-09-23"
+}]
+
+export const vaapen = [
+    {
+        lopenr: 1,
+        vaapennr: "15461234",
+        navn: "Tikka",
+        kaliber: "30-06",
+        godkjent: true,
+        godkjentdato: "24-10-2019"
+    },
+    {
+        lopenr: 2,
+        vaapennr: "1234",
+        navn: "Sako",
+        kaliber: "6,5x55",
+        godkjent: false
+    }
+]
+
+export const jegerinfo = {
+    navn: "Jeger Jegersen",
+    fodselsaar: 1985,
+    jegernr: "00399561",
+    jaktaar: "2019/2020",
+    betalt: true,
+    jakttype: "Alt vilt, unntatt elg, hjort og villrein",
+    "lisens-arter": "Jerv, Gaupe, Bjørn, Ulv",
+    "vaapenkort-url": "",
+    proveskudd: proveskudd,
+        
+    skyteprove: skyteprover,
+    vaapen: vaapen
+}
